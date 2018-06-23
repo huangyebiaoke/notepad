0cb70cacf31576eaa2efa15d171cdb5218a7e5ad
huangyebiaoke <huangyebiaoke@outlook.com>
2018-06-23 19:26:10 +0800

    增加快捷键,美化界面

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # notepad
+
 a simple notepad based on java GUI
 
+
+
 ###usage:
+
 1. `git clone git@github.com:huangyebiaoke/notepad.git`
+
 2. open eclipse->file->import->Git->project from git
 
+
+
 ###other:
+
 1. base on javax.swing
+
 2. write code with eclipse
+
 3. class in src dir..
+
+
 
 ![demo](http://madeai.cn/usr/uploads/demo.png "demo")
--- src/cn/madeai/Notepad.java
+++ src/cn/madeai/Notepad.java
@@ -2,9 +2,13 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -22,6 +26,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
 
 public class Notepad extends JFrame implements ActionListener {
@@ -30,23 +35,26 @@ public class Notepad extends JFrame implements ActionListener {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	JMenuBar menuBar;
+	private JMenuBar menuBar;
 
-	JMenu menu0,menu1,menu2;
+	private JMenu menu0,menu1,menu2;
 
 	// MenuItem item0_0,item0_1,item0_2,item0_3, item1_0,item1_1;
 
-	JTextArea textArea;
+	private JTextArea textArea;
 	// 文件路径的全局变量;
-	String absolutePath;
+	private String absolutePath;
 
-	public Notepad(String s, int x, int y, int w, int h) {
+	public Notepad(String s, int w, int h) {
 
 		init(s);
-
-		setLocation(x, y);
-
+		
 		setSize(w, h);
+		
+		//居中显示JFrame;
+		Toolkit tk=this.getToolkit();
+		Dimension dim=tk.getScreenSize();
+		this.setLocation((int)(dim.getWidth()-this.getWidth())/2,(int)(dim.getHeight()-this.getHeight())/2);
 
 		setVisible(true);
 		
@@ -55,7 +63,7 @@ public class Notepad extends JFrame implements ActionListener {
 
 	}
 
-	void init(String s) {
+	protected void init(String s) {
 
 		setTitle(s);
 		
@@ -68,10 +76,10 @@ public class Notepad extends JFrame implements ActionListener {
 		String[][] itemTitle = { 
 				{ "新建", "打开", "保存", "另存", "退出" },
 				{"剪切","复制","黏贴"},
-				{ "作者：小明", "版本：v1.0" }
+				{ "作者：小明", "版本：v1.1" }
 			};
 
-		menu0 = new JMenu("文件");
+		menu0 = new JMenu("文件(F)");
 		JMenuItem[] item0 = new JMenuItem[itemTitle[0].length];
 		for (int i = 0; i < item0.length; i++) {
 			item0[i] = null;
@@ -82,9 +90,15 @@ public class Notepad extends JFrame implements ActionListener {
 			item0[i].addActionListener(this);
 			item0[i].setActionCommand(i + "");
 		}
+		menu0.setMnemonic('f');
+		item0[0].setAccelerator(KeyStroke.getKeyStroke('N', java.awt.Event.CTRL_MASK));
+		item0[1].setAccelerator(KeyStroke.getKeyStroke('O', java.awt.Event.CTRL_MASK));
+		item0[2].setAccelerator(KeyStroke.getKeyStroke('S', java.awt.Event.CTRL_MASK));
+		item0[3].setAccelerator(KeyStroke.getKeyStroke('S', KeyEvent.CTRL_MASK+KeyEvent.ALT_MASK));
+		item0[4].setAccelerator(KeyStroke.getKeyStroke('X', KeyEvent.CTRL_MASK+KeyEvent.ALT_MASK));
 		menuBar.add(menu0);
 
-		menu1 = new JMenu("编辑");
+		menu1 = new JMenu("编辑(E)");
 		JMenuItem[] item1 = new JMenuItem[itemTitle[1].length];
 		for (int i = 0; i < item1.length; i++) {
 			item1[i] = null;
@@ -95,9 +109,14 @@ public class Notepad extends JFrame implements ActionListener {
 			item1[i].addActionListener(this);
 			item1[i].setActionCommand(i + item0.length + "");
 		}
+		menu1.setMnemonic('e');
+		item1[0].setAccelerator(KeyStroke.getKeyStroke('X', java.awt.Event.CTRL_MASK));
+		item1[1].setAccelerator(KeyStroke.getKeyStroke('C', java.awt.Event.CTRL_MASK));
+		item1[2].setAccelerator(KeyStroke.getKeyStroke('V', java.awt.Event.CTRL_MASK));
 		menuBar.add(menu1);
 		
-		menu2 = new JMenu("关于");
+		
+		menu2 = new JMenu("关于(A)");
 		JMenuItem[] item2 = new JMenuItem[itemTitle[2].length];
 		for (int i = 0; i < item2.length; i++) {
 			item2[i] = null;
@@ -108,16 +127,36 @@ public class Notepad extends JFrame implements ActionListener {
 			item2[i].addActionListener(this);
 			item2[i].setActionCommand(i + item0.length +item1.length+ "");
 		}
+		menu2.setMnemonic('a');
+		item2[0].setToolTipText("access my blog?");
+		item2[1].setToolTipText("access the project website?");
 		menuBar.add(menu2);
 
 		setJMenuBar(menuBar);
+		
+		ImageIcon background=new ImageIcon("resources\\background.png");
 
-		textArea = new JTextArea(10, 15);
+		textArea = new JTextArea(10, 15) {
+			   {
+				    setOpaque(false); // 设置透明
+				   }
+
+				   protected void paintComponent(Graphics g) {
+				    g.drawImage(background.getImage(), 0, 0, this.getWidth(),this.getHeight(),this);
+				    super.paintComponent(g);
+				   }
+		};
 		textArea.setTabSize(4);
 		textArea.setFont(new Font("标楷体", Font.BOLD, 16));
 		textArea.setLineWrap(true);// 激活自动换行功能
 		textArea.setWrapStyleWord(true);// 激活断行不断字功能
-		textArea.setBackground(Color.white);
+		//防止背景图片加载不出来,背景设为黑色;
+		if(background==null) {
+			textArea.setBackground(Color.BLACK);
+		}
+		textArea.setForeground(Color.WHITE);
+		textArea.setCaretColor(Color.WHITE);
+//		textArea.getCaret().setBlinkRate(WAIT_CURSOR);
 
 		this.getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
 
@@ -194,7 +233,9 @@ public class Notepad extends JFrame implements ActionListener {
 			}
 		}
 	}
-
+	/*
+	 * 另存,第一次一定要另存;
+	 */
 	protected void savaAs(String path, String text) {
 		File f = new File(path);
 		if (!f.exists())
@@ -210,11 +251,11 @@ public class Notepad extends JFrame implements ActionListener {
 		writeFile(f.getAbsolutePath(), text);
 	}
 	
-	public void goBlog() {
+	protected void goWebsite(String url) {
 		if (java.awt.Desktop.isDesktopSupported()) {
 			try {
 				// 创建一个URI实例
-				java.net.URI uri = java.net.URI.create("https://blog.madeai.cn");
+				java.net.URI uri = java.net.URI.create(url);
 				// 获取当前系统桌面扩展
 				java.awt.Desktop dp = java.awt.Desktop.getDesktop();
 				// 判断系统桌面是否支持要执行的功能
@@ -234,13 +275,11 @@ public class Notepad extends JFrame implements ActionListener {
 		/*
 		 * if(e.getSource()==item0_0) { textArea.setText("hello world!!"); }
 		 */
-
 		switch (e.getActionCommand().charAt(0)) {
 		case '0':
 			Date d = new Date();
 			SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			textArea.setText(sf.format(d) + "\r\n\t");
-			System.out.println(e.getActionCommand().charAt(0));
 			break;
 		case '1':
 			char[] text = readFile(choseFile("E:\\notebook\\"));
@@ -259,21 +298,25 @@ public class Notepad extends JFrame implements ActionListener {
 			dispose();
 			break;
 		case '5':
-			textArea.setText("功能待实现");
+			JOptionPane.showMessageDialog(null, "功能待实现,请使用Ctrl+X快捷键操作");
 			break;
 		case '6':
-			textArea.setText("功能待实现");
+			JOptionPane.showMessageDialog(null, "功能待实现,请使用Ctrl+C快捷键操作");
 			break;
 		case '7':
-			textArea.setText("功能待实现");
+			JOptionPane.showMessageDialog(null, "功能待实现,请使用Ctrl+V快捷键操作");
 			break;
 		case '8':
-			goBlog();
+			goWebsite("https://blog.madeai.cn");
+			break;
+		case '9':
+			goWebsite("https://github.com/huangyebiaoke/notepad");
 		}
 	}
 
 	/*
 	 * 内部类实现过滤文件类型;
+	 * 打开操作用的上;
 	 */
 	class TxtFileFilter extends FileFilter {
 		public String getDescription() {
@@ -288,6 +331,6 @@ public class Notepad extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-		Notepad notepad = new Notepad("记事本", 300, 300, 500, 250);
+		Notepad notepad = new Notepad("记事本", 640, 400);
 	}
 }
